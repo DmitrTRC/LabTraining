1a8ee6e00b572d984eef785dce62edb60099f9ef
Dev Spain <9984398@gmail.com>
2021-10-01 18:13:18 +0200

    Completed.

--- PartI/Lab_1_1/DEV/Lab_1_1_Simple.py
+++ PartI/Lab_1_1/DEV/Lab_1_1_Simple.py
@@ -1,7 +1,16 @@
 print('Laboratory work 1.1 measurements of the R-load.')
 
+measurements = []
+
 measurements_count = int(input('Enter number of measurements : '))
 
-for cur_measurement_number in range(1, measurements_count):
-    print(f'Enter measurement {cur_measurement_number}  -> ')
+for cur_measurement_number in range(1, measurements_count+1):
+    print(f'Enter value for measurement {cur_measurement_number} -> ', end='')
     measurement = float(input())
+    measurements.append(measurement)
+
+print('Results of the measurements: ')
+
+for index, cur_measurement_number in enumerate(measurements):
+    print (f'({index}): {cur_measurement_number} Ohms')
+
