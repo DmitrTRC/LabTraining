2dc46d4a6546327c06174145f59817fef203cd54
Dmitry <9984398@gmail.com>
2021-10-10 00:17:44 +0200

    File read completed. Not parsed yet.

--- DEV/iteration_size.cpp
+++ DEV/iteration_size.cpp
@@ -4,19 +4,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 
-std::vector<std::string> parseText(const std::string &text_buff) {
-
-    size_t start = 0;
-// look for end of sentence
-    size_t finish = text_buff.find_first_of(".!?", start);
-
-    while (finish != std::string::npos) {
-
-// end of sentence was found, do something here.
-
-// now find start of next sentence
-        start = text_buff.find_first_not_of(" \t\n", finish + 1);
+std::string ReadTextFile(const std::string &file_name) {
+    std::string text_buff;
+    std::ifstream fileDescriptor(file_name);
+    if (!fileDescriptor) {
+        std::cout << std::endl << "Fine " << file_name << " : not found" << std::endl;
     }
+    std::string next_string;
+
+    while (getline(fileDescriptor, next_string)) {
+        text_buff += next_string;
+    }
+
+    fileDescriptor.close();
+    return text_buff;
+}
+
+//std::vector<std::string> parseText(const std::string &text_buff) {
+//
+//    size_t start = 0;
+//// look for end of sentence
+//    size_t finish = text_buff.find_first_of(".!?", start);
+//
+//    while (finish != std::string::npos) {
+//
+//// end of sentence was found, do something here.
+//
+//// now find start of next sentence
+//        start = text_buff.find_first_not_of(" \t\n", finish + 1);
+//    }
+//}
+int main(){
+     std:: cout << ReadTextFile ("demo.txt");
 }
