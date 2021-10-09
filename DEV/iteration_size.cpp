970cba81d7245fe59b2f21114126e25bfe7bf36f
Dev Spain <9984398@gmail.com>
2021-10-10 00:53:18 +0200

    NC 2

--- DEV/iteration_size.cpp
+++ DEV/iteration_size.cpp
@@ -23,20 +23,22 @@ std::string ReadTextFile(const std::string &file_name) {
     return text_buff;
 }
 
-//std::vector<std::string> parseText(const std::string &text_buff) {
-//
-//    size_t start = 0;
-//// look for end of sentence
-//    size_t finish = text_buff.find_first_of(".!?", start);
-//
-//    while (finish != std::string::npos) {
-//
-//// end of sentence was found, do something here.
-//
-//// now find start of next sentence
-//        start = text_buff.find_first_not_of(" \t\n", finish + 1);
-//    }
-//}
-int main(){
-     std:: cout << ReadTextFile ("demo.txt");
+std::vector<std::string> parseText(const std::string &text_buff) {
+    std::string const delim{".:;!?"};
+    std::vector<std::string> res_text;
+    size_t begin, pos = 0;
+
+    while ((begin = text_buff.find_first_not_of(delim, pos)) != std::string::npos) {
+        pos = text_buff.find_first_of(delim, begin + 1);
+        res_text.push_back(text_buff.substr(begin, pos - begin));
+    }
+
+    return res_text;
+}
+
+int main() {
+    auto parsed_text = parseText( ReadTextFile("demo.txt"));
+    for( const auto &line : parsed_text ) {
+        std::cout << line << std::endl;
+    }
 }
