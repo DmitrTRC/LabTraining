ce7f72f8038533e4e91c3c0f9852f0b68975c556
Dmitry Morozov <9984398@gmail.com>
2021-11-14 21:15:36 +0300

    Lesson_8 Started. t1 : Refactored.

--- PartI/C++/Lesson_8/t1.cpp
+++ PartI/C++/Lesson_8/t1.cpp
@@ -1,28 +1,24 @@
-//
-// Created by moroz on 13.11.2021.
-//
-
 #include <iostream>
-
-using namespace std; //Bad !
-
 #include <vector>
 #include <algorithm>
+
+using namespace std;
 
 int main() {
     vector<int> a;
     string m;
     int n;
-    int suma = 0;
+    int summa = 0; //  int summa (0); int summa {0};
     cin >> m;
     cin >> n;
     int k = 1;
-    //for (int i = m.size() - 1; i >= 0; --i) {  Warning type conversion
-    for (size_t i = m.size() - 1; i >= 0; --i) { // Or use auto
 
-        suma += (m[i] - '0') * k;
+    for (size_t i = m.size(); i != 0; --i) {
+
+        summa += (m[i - 1] - '0') * k;
         k = k * n;
 
     }
-    cout << suma;
+
+    cout << summa;
 }
