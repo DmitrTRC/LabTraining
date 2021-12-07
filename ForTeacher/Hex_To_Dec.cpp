f2d9d03c1e39af34460768956121f63e691642a9
dmitrytrc <9984398@gmail.com>
2021-12-07 17:08:31 +0300

    Hex_To_Dec. bool IsHexInt added and tested.

--- ForTeacher/Hex_To_Dec.cpp
+++ ForTeacher/Hex_To_Dec.cpp
@@ -5,12 +5,33 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <bitset>
+#include <sstream>
+
+///Checks whether a std::string contains hexadecimal
+///can be converted to an int.
+///Returns true if possible, also returning this integer by referencing.
+///Returns false otherwise, setting the referenced int to zero.
+bool IsHexInt(const std::string &s, int &rInt) {
+    std::istringstream i(s);
+    i >> std::hex;
+    if (!(i >> rInt)) {
+        rInt = 0;
+        return false;
+    }
+    return true;
+}
 
 int main() {
     int x{0};
     while (true) {
+        std::string str_x;
         std::cout << "Enter hex number (or 0 to exit ) : ";
-        std::cin >> std::hex >> x;
+        std::cin >> str_x;
+        int x;
+
+        if (!IsHexInt(str_x, x)) std::throw_with_nested(std::runtime_error("Not a hex number"));
+
         if (not x) break;
         std::cout << std::dec << x << std::endl;
         std::bitset<32> bin_x(x);
