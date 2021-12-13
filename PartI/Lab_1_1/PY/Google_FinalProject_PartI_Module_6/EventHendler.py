6d15d14ab6268f4b3926a19bd5fa9a2fbea9e313
dmitrytrc <9984398@gmail.com>
2021-12-13 20:29:45 +0300

    Final Project Part I. Mod 6.
    Stage 1 : Tested. Dictionary creates properly.

--- PartI/Lab_1_1/PY/Google_FinalProject_PartI_Module_6/EventHendler.py
+++ PartI/Lab_1_1/PY/Google_FinalProject_PartI_Module_6/EventHendler.py
@@ -1,8 +1,11 @@
+from Event import Event
+
+
 def get_event_date(event):
-    return event.date()
+    return event.date
 
 
-def current_users(evetns):
+def current_users(events):
     evetns.sort(key=get_event_date)
     machines = {}
     for event in events:
@@ -20,3 +23,17 @@ def generate_report(machines):
         if len(users) > 0:
             user_list = ', '.join(users)
             print(f'{machine}: {user_list}')
+
+
+events = [
+    Event('12:00:00', 'login', 'moxa', 'user1'),
+    Event('13:00:00', 'logout', 'moxa', 'user1'),
+    Event('12:00:00', 'login', 'moxa', 'user2'),
+    Event('13:00:00', 'logout', 'moxa', 'user2'),
+    Event('12:00:00', 'login', 'moxa', 'user3'),
+    Event('13:00:00', 'logout', 'moxa', 'user3'),
+    Event('12:00:00', 'login', 'moxa', 'user4')
+    ]
+
+users = current_users(events)
+print(users)
