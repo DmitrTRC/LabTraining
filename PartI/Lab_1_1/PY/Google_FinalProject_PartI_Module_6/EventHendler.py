8218e04a76ade04f22f26f20c22cdc63a45b9719
dmitrytrc <9984398@gmail.com>
2021-12-13 19:34:03 +0300

    Final Project Part I. Mod 6.
    print_report () :
    refactored.

--- PartI/Lab_1_1/PY/Google_FinalProject_PartI_Module_6/EventHendler.py
+++ PartI/Lab_1_1/PY/Google_FinalProject_PartI_Module_6/EventHendler.py
@@ -19,4 +19,4 @@ def generate_report(machines):
     for machine, users in machines.items():
         if len(users) > 0:
             user_list = ', '.join(users)
-            print('{}: {}'.format(machine, user_list))
+            print(f'{machine}: {user_list}')
