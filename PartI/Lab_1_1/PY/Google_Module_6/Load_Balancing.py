bf1c5427757fdcd0b47a34ce19b85c95a5e0662c
dmitrytrc <9984398@gmail.com>
2021-12-13 18:28:46 +0300

    Load_Balancing.py : Implementation

--- PartI/Lab_1_1/PY/Google_Module_6/Load_Balancing.py
+++ PartI/Lab_1_1/PY/Google_Module_6/Load_Balancing.py
@@ -1,4 +1,7 @@
-#Begin Portion 2#
+from server import Server
+
+
+# Begin Portion 2#
 class LoadBalancing:
     def __init__(self):
         """Initialize the load balancing system with one server"""
@@ -10,24 +13,30 @@ class LoadBalancing:
         server = random.choice(self.servers)
         # Add the connection to the dictionary with the selected server
         # Add the connection to the server
+        self.connections[connection_id] = server
+        server.add_connection(connection_id)
 
     def close_connection(self, connection_id):
         """Closes the connection on the the server corresponding to connection_id."""
         # Find out the right server
         # Close the connection on the server
         # Remove the connection from the load balancer
+        server = self.connections[connection_id]
+        server.close_connection(connection_id)
+        del self.connections[connection_id]
 
     def avg_load(self):
         """Calculates the average load of all servers"""
         # Sum the load of each server and divide by the amount of servers
-        return 0
+        return sum(server.load for server in self.servers) / len(self.servers)
 
     def ensure_availability(self):
         """If the average load is higher than 50, spin up a new server"""
-        pass
+        if self.avg_load() > 50:
+            self.servers.append(Server())
 
     def __str__(self):
         """Returns a string with the load for each server."""
         loads = [str(server) for server in self.servers]
         return "[{}]".format(",".join(loads))
-#End Portion 2#
+# End Portion 2#
