bacae8926a2f991a933702eb30378d89c6477293
dmitrytrc <9984398@gmail.com>
2021-12-13 20:19:34 +0300

    Final Project Part I. Mod 6.
    Class  Event : Daraft Implemented.

---
+++ PartI/Lab_1_1/PY/Google_FinalProject_PartI_Module_6/Event.py
@@ -0,0 +1,7 @@
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.event_date = event_date
+        self.event_type = event_type
+        self.machine_name = machine_name
+        self.user = user
+
