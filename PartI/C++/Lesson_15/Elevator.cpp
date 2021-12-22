726dce5bf27c1cd8593bc7dd7d6242225cf603c9
dmitrytrc <9984398@gmail.com>
2021-12-22 20:00:02 +0300

    Test added for Elevator class.

--- PartI/C++/Lesson_15/Elevator.cpp
+++ PartI/C++/Lesson_15/Elevator.cpp
@@ -1,6 +1,10 @@
 //
 // Created by Dmitry Morozov on 22.12.2021.
 //
+#include <iostream>
+#include <cassert>
+
+
 class Elevator {
 public:
     Elevator (int _maxFloor, int _minFloor, int _currentFloor) { //Your code here
@@ -15,7 +19,7 @@ public:
     void move (int floor) { //Your code here
     }
 
-    void showFloor () {//Your code here
+    int getCurrentFloor () {//Your code here
     }
 
 private:
@@ -24,17 +28,38 @@ private:
     int currentFloor;
 };
 
-int main () {
-    Elevator elevator (10, 1, 1);
-    elevator.move (5);
-    elevator.showFloor ();
-    elevator.moveUp ();
-    elevator.showFloor ();
-    elevator.moveDown ();
-    elevator.moveDown ();
-    elevator.showFloor ();
-    elevator.move (3);
-    elevator.showFloor ();
+//Tests for Elevator class
+void test () {
+    Elevator elevator (10, 1, 3);
 
+    elevator.move (5);
+    std::cout << elevator.getCurrentFloor () << std::endl; // Has to print 5
+    assert(elevator.getCurrentFloor () == 5);
+
+    elevator.moveUp ();
+    std::cout << elevator.getCurrentFloor () << std::endl; // Has to print 6
+    assert(elevator.getCurrentFloor () == 6);
+
+    elevator.moveDown ();
+    elevator.moveDown ();
+    std::cout << elevator.getCurrentFloor () << std::endl; // Has to print 4
+    assert(elevator.getCurrentFloor () == 4);
+
+    elevator.move (3);
+    std::cout << elevator.getCurrentFloor () << std::endl; // Has to print 3
+    assert(elevator.getCurrentFloor () == 3);
+
+    elevator.move (10);
+    std::cout << elevator.getCurrentFloor () << std::endl; // Has to print 10
+    assert(elevator.getCurrentFloor () == 10);
+
+    std::cout << "Test passed!" << std::endl;
+
+}
+
+int main () {
+    // Just call test() function
+    test ();
     return 0;
 }
+
