7cdec0062a8852cde05b71b396e92c8f7a20c3a6
dmitrytrc <9984398@gmail.com>
2021-12-31 21:13:14 +0300

    coins()  : Vector solution added.

--- OlympicTraining/coins.cpp
+++ OlympicTraining/coins.cpp
@@ -40,25 +40,63 @@
 Входные данные: 999
 Выходные данные: 143
  **/
-
 #include <iostream>
+#include <vector>
 
 
 int coins (int n) {
     int count = 0;
-    count += n / 7;
-    n %= 7;
-    count += n / 5;
-    n %= 5;
-    count += n / 4;
-    n %= 4;
-    count += n / 1;
+    while (n > 0) {
+        if (n >= 7) {
+            n -= 7; // n = n -7
+            count++;
+        } else if (n >= 5) {
+            n -= 5;
+            count++;
+        } else if (n >= 4) {
+            n -= 4;
+            count++;
+        } else if (n >= 1) {
+            n -= 1;
+            count++;
+        }
+    }
     return count;
 }
+
+int coins_by_divide (int n) {
+    int count = 0;
+    count += n / 7;
+    n = n % 7;
+    count += n / 5;
+    n = n % 5;
+    count += n / 4;
+    n = n % 4;
+    count += n / 1;
+    n = n % 1;
+
+    return count;
+
+}
+
+
+int coins_custom (int n, std::vector<int> arr = {1, 5, 4, 7}) {
+    int count = 0;
+    sort (arr.begin (), arr.end (), [] (int a, int b) { return a > b; });
+    for (auto i: arr) {
+        count += n / i;
+        n %= i;
+    }
+    return count;
+}
+
 
 int main () {
     int n;
     std::cin >> n;
     std::cout << coins (n) << std::endl;
+    std::cout << coins_by_divide (n) << std::endl;
+    std::cout << coins_custom (n) << std::endl;
+    std::cout << coins_custom (n, {1, 4, 5, 3, 7}) << std::endl;
     return 0;
 }
