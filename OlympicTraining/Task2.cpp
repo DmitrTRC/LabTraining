5aa3cd023d153e07efee4031b32dd5c608d47efc
dmitrytrc <9984398@gmail.com>
2022-01-03 00:34:41 +0300

    NC

--- OlympicTraining/Task2.cpp
+++ OlympicTraining/Task2.cpp
@@ -37,24 +37,31 @@
 Выходные данные: 101935
  */
 
-int num_digets (int n) {
+int num_digits (int n) {
     int count = 0;
+    if (n < 0) {
+        n = -n;
+    }
     while (n > 0) {
         n /= 10;
         count++;
     }
+
     return count;
 }
 
 int get_total_pages_numbers (int total_pages_symbols) {
     int current_pages_symbols = 0;
     int current_pages_number = 0;
+    if (total_pages_symbols) {
 
-    while (current_pages_symbols < total_pages_symbols) {
-        current_pages_symbols += num_digets (current_pages_number);
-        current_pages_number++;
+        while (current_pages_symbols < total_pages_symbols) {
+            current_pages_symbols += num_digits (current_pages_number);
+            current_pages_number++;
+        }
+        current_pages_number--;
     }
-    return current_pages_number-1;
+    return current_pages_number;
 }
 
 void test_get_total_pages () {
@@ -64,11 +71,30 @@ void test_get_total_pages () {
     assert(get_total_pages_numbers (1314) == 474);
     assert(get_total_pages_numbers (10001) == 2777);
     assert(get_total_pages_numbers (500505) == 101935);
-    std::cout << "All tests passed" <<std::endl; // Trying to use :smile:
+    assert(get_total_pages_numbers (0) == 0);
+
+    std::cout << "get_total_pages_numbers() tests passed" << "✌️" << std::endl; // Trying to use :smile:
 
 }
 
+void test_num_digits () {
+    assert(num_digits (13) == 2);
+    assert(num_digits (25) == 2);
+    assert(num_digits (53) == 2);
+    assert(num_digits (1314) == 4);
+    assert(num_digits (10001) == 5);
+    assert(num_digits (500505) == 6);
+    assert(num_digits (0) == 0);
+    assert(num_digits (-1) == 1);
+    assert(num_digits (-10) == 2);
+    assert(num_digits (-100) == 3);
+
+
+    std::cout << "num_digits() tests passed" << "✌️" << std::endl; // Trying to use :smile:
+}
+
 int main () {
+    test_num_digits ();
     test_get_total_pages ();
     return 0;
 }
